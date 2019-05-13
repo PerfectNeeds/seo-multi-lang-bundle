fd54de9e8cbdded4fbc3d633fb2dbe5a514b8f33
Peter Nassef <peter.nassef@gmail>
2019-05-13 10:30:50 +0200

    change DependencyInjection class name

--- DependencyInjection/PNSeoExtension.php
+++ DependencyInjection/PNSeoExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class SeoExtension extends Extension {
+class PNSeoExtension extends Extension {
 
     /**
      * {@inheritdoc}
