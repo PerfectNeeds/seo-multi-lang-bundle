e4d38453b6c43dbebb3d0d74023c6056a8e4adbc
Peter Nassef <peter.nassef@gmail.com>
2022-03-15 13:44:36 +0200

    Enhance Redirect Listener

--- Listener/RedirectListener.php
+++ Listener/RedirectListener.php
@@ -5,6 +5,7 @@ namespace PN\SeoBundle\Listener;
 use Doctrine\ORM\EntityManagerInterface;
 use PN\SeoBundle\Entity\Redirect404;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -23,7 +24,7 @@ class RedirectListener
     public function onKernelRequest(RequestEvent $event)
     {
         if ($event->isMainRequest()) {
-            $url = $event->getRequest()->getUri();
+            $url = $this->getUrl($event->getRequest());
             if (strpos($url, "/admin/") === false) {
                 $redirectPage = $this->em->getRepository(Redirect404::class)->findOneBy(["from" => $url]);
                 if ($redirectPage) {
@@ -33,4 +34,15 @@ class RedirectListener
         }
     }
 
+    private function getUrl(Request $request):string
+    {
+        $url = $request->getUri();
+
+        if (strpos($url, "?") !== false) {
+            $queryParameters = substr($request->getRequestUri(), strpos($request->getRequestUri(), "?"));
+            return substr($url, 0, strpos($url, "?")).$queryParameters;
+        }
+
+        return $url;
+    }
 }
